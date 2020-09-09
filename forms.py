3d9a093177f6221ba21e1d157ab0e6c12f0859ae
Kamil Kamaliev <gregoryfroydentaler23@gmail.com>
2020-09-09 07:03:41 +0000

    +add_pet +list_pets html

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField
+from wtforms import StringField, IntegerField, SubmitField, TextAreaField
 from wtforms.validators import InputRequired, NumberRange
 
 class AddForm(FlaskForm):
@@ -7,7 +7,7 @@ class AddForm(FlaskForm):
     pet_name = StringField('Name of the pet: ', [InputRequired(message="Please, provide pet name")])
     pet_type = StringField('Pet type: ', [InputRequired(message="Please, provide pet type")])
     pet_age = IntegerField('Pet age (in years): ', [InputRequired(message="Please, provide pet age"), NumberRange(0, 100)])
-    additional = StringField('Additional information: ')
+    additional = TextAreaField('Additional information: ')
     owner_name = StringField('Owner name: ', [InputRequired(message="Please, provide owner name")])
     owner_email = StringField('Contact email: ', [InputRequired(message="Please, provide owner contact email")])
     owner_phone = StringField('Phone number: ', [InputRequired(message="Please, provide owner contact phone")])
